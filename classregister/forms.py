3a31d2ef280f93212badde9b17fe27c3722c886b
jiss02 <olo5112olo@naver.com>
2019-11-15 21:23:25 +0900

    class crud

---
+++ classregister/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Class
+
+class ClassForm(forms.ModelForm):
+    class Meta:
+        model = Class
+        fields = ('class_title', 'summary', 'describe', 'price', 
+        'date', 'category', 'current_number', 'max_number', 
+        'img1', 'img2', 'img3', 'img4', 'img5')
--- storeregister/models.py
+++ storeregister/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Store(models.Model):
     REGION_CHOICES = (
-		('GN', '강남구'),
-        ('SC', '서초구'),
-        ('YS', '용산구'),
+      ('GN', '강남구'),
+      ('SC', '서초구'),
+      ('YS', '용산구'),
     )
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     store_title = models.CharField(max_length=100)
     region = models.CharField(max_length=200, choices=REGION_CHOICES)
     address = models.TextField()
@@ -16,3 +18,7 @@ class Store(models.Model):
     describe = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
+class Recommend(models.Model) :
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)    
