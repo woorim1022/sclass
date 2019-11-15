c319413d28fc554724238d8352264f2b2a2774c8
3jine33 <3jine33@likelion.org>
2019-11-16 00:55:32 +0900

    store

--- storeregister/admin.py
+++ storeregister/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from .models import Store, Recommend
 # Register your models here.
 admin.site.register(Store)
-<<<<<<< HEAD
 admin.site.register(Recommend)
-=======
-admin.site.register(Recommend)
->>>>>>> origin/master
