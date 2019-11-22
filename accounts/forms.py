784f1ea68c230ecdbfb83d429b5d51e2305f6d94
jiss02 <olo5112olo@naver.com>
2019-11-22 20:02:39 +0900

    html and style

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,9 +5,38 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'seller', 'phone')
-
-
-
-
-
-
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={
+                    'class': "form-control input display-7",
+                    'required': "required",
+                    'id': "name-form3-2w",
+                    'placeholder': '이름',
+                }
+            ),
+            'last_name': forms.TextInput(
+                attrs={
+                    'class': "form-control input display-7",
+                    'required': "required",
+                    'id': "name-form3-2w",
+                    'placeholder': '성',
+                }
+            ),
+            'email': forms.TextInput(
+                attrs={
+                    'type': 'email',
+                    'class': "form-control input display-7",
+                    'required': "required",
+                    'id': "name-form3-2w",
+                    'placeholder': '이메일',
+                }
+            ),
+            'phone': forms.TextInput(
+                attrs={
+                    'class': "form-control input display-7",
+                    'required': "required",
+                    'id': "name-form3-2w",
+                    'placeholder': '전화번호',
+                }
+            ),
+        }
